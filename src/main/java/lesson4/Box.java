d4bfe01b9da37b27283649dd898acadf26f8e994
babai <louk@mail.ru>
2022-08-08 10:54:55 +0300

    закончил

--- src/main/java/lesson4/Box.java
+++ src/main/java/lesson4/Box.java
@@ -6,6 +6,10 @@ import java.util.stream.Collectors;
 public class Box<T extends Fruit> {
     private ArrayList<T> box = new ArrayList<>();
 
+    public Box(T fruit, int number) {
+        for(int i = 1; i <= number; i++ ) this.box.add(fruit);
+    }
+
     public void put(T fruit) {
         this.box.add(fruit);
     }
@@ -13,9 +17,22 @@ public class Box<T extends Fruit> {
     public ArrayList<T> getBox() {
         return box;
     }
+
     public double getWeight(){
         return box.stream().mapToDouble(Fruit::getWeight).sum();
     }
 
-    public boolean compare(Box<T> box) implements Comp
+    public boolean compare(Box box) {
+        final double threshold = 0.0001;
+        System.out.println(this.getWeight());
+        System.out.println(box.getWeight());
+        if(Math.abs(this.getWeight() - box.getWeight()) < threshold) return true;
+        return false;
+    }
+
+    public void join(Box<T> box) {
+        if(this == box) return;
+        box.getBox().addAll(this.box);
+        this.box.clear();
+    }
 }
--- src/main/java/lesson4/Lesson4.java
+++ src/main/java/lesson4/Lesson4.java
@@ -14,9 +14,26 @@ public class Lesson4 {
 
         Fruit apple = new Apple();
         System.out.println(apple.getWeight());
-        Box<Apple> apples = new Box<>();
+        Box<Apple> apples = new Box<>(new Apple(),6);
         apples.put((Apple)apple);
-        System.out.println(apples.getWeight());
+        System.out.println("Коробка с яблоками: " + apples.getWeight());
+
+        Box<Apple> apples1 = new Box<>(new Apple(),17);
+        apples1.put((Apple)apple);
+        System.out.println("Коробка с яблоками1: " + apples1.getWeight());
+
+        Box<Orange> oranges = new Box<>(new Orange(),11);
+        oranges.put(new Orange());
+        System.out.println("Коробка с апельсинами: " + oranges.getWeight());
+
+        System.out.println(apples.compare(oranges));
+        System.out.println(apples1.compare(oranges));
+
+        apples1.join(apples);
+        System.out.println("Коробка с апельсинами: " + oranges.getWeight());
+        System.out.println("Коробка с яблоками: " + apples.getWeight());
+        System.out.println("Коробка с яблоками1: " + apples1.getWeight());
+
     }
     public static void changeArray(Object [] array, int i, int j){
         if(i == j) return;
